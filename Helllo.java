e5b9ec61a196398a74155bbae9fe32628894ddbc
Arun Pillai <55800842+arunpillai6@users.noreply.github.com>
2019-10-15 19:43:57 +0530

    Update the hello.java
    Hello.java updated

--- Helllo.java
+++ Helllo.java
@@ -5,5 +5,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
+     // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
     } 
 } 
